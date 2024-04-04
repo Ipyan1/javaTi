1a8f43601243ed231a998d3d8665bc728a72e343
PAMELA OLIVEIRA SANTOS <pamela.osantos3@senacsp.edu.br>
2024-04-03 21:56:21 -0300

    primeiro commit

--- Javascript/funcoes.js
+++ Javascript/funcoes.js
@@ -74,29 +74,84 @@ function tabuada(){
 function potencia(){
     var num1;
     var num2;
+    var res;
 
     num1 = parseInt(document.getElementById("tpNum").value);
     num2 = parseInt(document.getElementById("tsNum").value);
 
-    var resNum1 = "A potencia dos números é " + num1;
-    var resNum2 = "A potencia  dos números é " + num2;
-
-    document.getElementById ( )
-
-
-
+    res= Math.pow(num1,num2)
+    
+    document.getElementById("resultado").innerHTML= "A potencia é: " + res;
 
 }// fim do método
 
-
 //Faça uma funçao para calcular a raiz dos dois números
 
+function raiz(){
+    var num1;
+    var num2;
+    var res;
+
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+
+     
+    res = "Raiz de: " + num1 + "=" + Math.sqrt(num1) + "\nRaiz de: " + num2 + "=" + Math.sqrt(num2);
+
+    document.getElementById("resultado").innerHTML = res;
+}// fim do método0
 
 //Faça uma função para calcular a fórmula de bhaskara
+function bhaskara(){
+    var a;
+    var b;
+    var c;
+    var res;
+    var res2;
+
+    a = parseInt(document.getElementById("tpNum").value);
+    b = parseInt(document.getElementById("tsNum").value);
+    c = parseInt(document.getElementById("tpNum").value);
+    
+    // calular Delta
+    delta=Math.pow(b,2) - 4* a * c;
+    
+    //calcular res e res2 se delta não for negativo
+
+    if(delta>=0){
+        res = (-b + Math.sqrt(delta))/(2*a);
+        res2 =(-b - Math.sqrt(delta))/(2*a);
+
+    }else{
+        res = "Impossível calcular x1, pois o delta é negativo";
+        res2= "Impossivel calcular x2, pois o delta é negativo";
+    }
+    //Mostrar na tela
+    document.getElementById("resultado").innerHTML = "\nDelta: " + delta + "\n\nres: " + res + "\nres2: " + res2;
+
+}// fim do método
+
+// Faça um programa que peça ao usuário um numero e imprima se é par ou impar
+
+function parImpar(){
+
+    var num1;
+    var num2;
+    var num3;
+    var res1;
+    var res2;
+    var res3;
+
+    num1 = parseInt(document.getElementById("tpNum").value);
+    num2 = parseInt(document.getElementById("tsNum").value);
+    num3 = parseInt(document.getElementById("ttnum").value);
+    
 
 
-// Faça um programa que peça ao usuário um numero e 
-//imprima se é par ou impar
+
+
+}//fim do método
+
 
 
 // Faça um programa que peça ao usário um número e imprima
